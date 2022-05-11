d81c32bc87b27ec9c0479a3cf3170f39fc28f43d
Andrew Whitaker <aawhitaker@gmail.com>
2022-05-11 15:28:38 -0400

    Refactoring integration tests for access

---
+++ integrationtests/access_test.go
@@ -0,0 +1,66 @@
+package integrationtests
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/maxatome/go-testdeep/helpers/tdhttp"
+	"github.com/maxatome/go-testdeep/td"
+	"github.com/stretchr/testify/suite"
+)
+
+type accessSuite struct {
+	suite.Suite
+}
+
+func TestAccess(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	suite.Run(t, new(accessSuite))
+}
+
+func (suite *accessSuite) BeforeTest(suiteName, testName string) {
+	TestContext.BeforeTest()
+}
+
+func (suite *accessSuite) TestAccessWithValidSlugReturns301() {
+	t := suite.T()
+
+	testServer := TestContext.server
+	testAPI := tdhttp.NewTestAPI(t, testServer)
+
+	var slug string
+
+	testAPI.PostJSON("/api/v1/shorturls", gin.H{"long_url": "https://www.cloudflare.com"}).
+		CmpStatus(http.StatusCreated).
+		CmpJSONBody(
+			td.JSON(
+				`{
+				   "short_url": "$shortUrl",
+					 "slug": "$slug"
+				 }`,
+				td.Tag("shortUrl", td.Ignore()),
+				td.Tag("slug", td.Catch(&slug, td.Ignore())),
+			),
+		)
+
+	testAPI.Get(fmt.Sprintf("/%s", slug)).
+		CmpStatus(http.StatusMovedPermanently).
+		CmpHeader(http.Header{
+			"Location":      []string{"https://www.cloudflare.com"},
+			"Cache-Control": []string{"private,max-age=0"},
+		})
+}
+
+func (suite *accessSuite) TestAccessWithInvalidSlugReturns404() {
+	t := suite.T()
+
+	testServer := TestContext.server
+	testAPI := tdhttp.NewTestAPI(t, testServer)
+
+	testAPI.Get("/invalid").CmpStatus(http.StatusNotFound)
+}
---
+++ integrationtests/api_delete_test.go
@@ -0,0 +1,77 @@
+package integrationtests
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/maxatome/go-testdeep/helpers/tdhttp"
+	"github.com/maxatome/go-testdeep/td"
+	"github.com/stretchr/testify/suite"
+)
+
+type deleteSuite struct {
+	suite.Suite
+}
+
+func TestDelete(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	suite.Run(t, new(deleteSuite))
+}
+
+func (suite *deleteSuite) BeforeTest(suiteName, testName string) {
+	TestContext.BeforeTest()
+}
+
+func (suite *deleteSuite) TestDeleteWithValidSlugReturns204() {
+	t := suite.T()
+
+	testServer := TestContext.server
+	testAPI := tdhttp.NewTestAPI(t, testServer)
+
+	var slug string
+
+	testAPI.PostJSON("/api/v1/shorturls", gin.H{"long_url": "https://www.google.com"}).
+		CmpStatus(http.StatusCreated).
+		CmpJSONBody(
+			td.JSON(
+				`{
+				   "short_url": "$shortUrl",
+				   "slug": "$slug"
+				 }`,
+				td.Tag("slug", td.Catch(&slug, td.Ignore())),
+				td.Tag("shortUrl", td.Ignore()),
+			),
+		)
+
+	testAPI.Delete(fmt.Sprintf("/api/v1/shorturls/%s", slug), nil).
+		CmpStatus(http.StatusNoContent)
+
+	testAPI.Get(fmt.Sprintf("/%s", slug)).CmpStatus(http.StatusNotFound)
+}
+
+func (suite *deleteSuite) TestDeleteWithInvalidSlugReturns404() {
+	t := suite.T()
+
+	testServer := TestContext.server
+	testAPI := tdhttp.NewTestAPI(t, testServer)
+
+	testAPI.Delete("/api/v1/shorturls/invalid", nil).
+		CmpStatus(http.StatusNotFound).
+		CmpJSONBody(
+			td.JSON(
+				`{
+				   "errors": [
+					   {
+						   "field": "Slug",
+							 "reason": "not found"
+						 }
+					 ],
+				 }`,
+			),
+		)
+}
